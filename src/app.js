264d44e9cf4be64c8a96d418bda4f0c3857eb4ff
palm <palm12345689@gmail.com>
2025-04-23 10:18:03 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const dayOfTheWeek = (date = new Date()) => {
     return days[date.getDay()];
 }
 
+const monthOfTheYear =(date =new Date()) => {
+    const month = ['January' , 'februrary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return month[date.getMonth()];
+}
+
+
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
